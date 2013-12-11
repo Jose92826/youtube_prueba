777511ee2df78bdce1f6af2ff69d653e5ed72ff7
ayd.1 <jose92826@hotmail.com>
2013-12-10 22:12:10 -0600

    Funcion de prueba.

---
+++ youtube_prueba/src/youtube_prueba/base.java
@@ -0,0 +1,15 @@
+package youtube_prueba;
+
+public class base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		suma(1,1);
+	}
+	
+	public static void suma(int x, int y){
+		System.out.println(x+y);
+	}
+
+}
